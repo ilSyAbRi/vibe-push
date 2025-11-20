05ea8a3255ae1fb00b860eecd1d2396ac06a4389
ilSyAbRi <iliasssyabri1@gmail.com>
2025-11-20 18:54:03 +0100

    Implement a simple variadic logger handling %s, %d, %% with log levels INFO, SUCCESS, WARNING.

--- variadic_log_msg.c
+++ variadic_log_msg.c
@@ -6,7 +6,7 @@
 /*   By: ilsyabri <ilsyabri@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/20 18:05:44 by ilsyabri          #+#    #+#             */
-/*   Updated: 2025/11/20 18:26:36 by ilsyabri         ###   ########.fr       */
+/*   Updated: 2025/11/20 18:53:39 by ilsyabri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 void	ft_putchar(char c)
 {
-	write(1,&c,1);
+	write(1, &c, 1);
 }
 
 void	ft_putstr(char *str)
@@ -25,16 +25,16 @@ void	ft_putstr(char *str)
 	i = 0;
 	while (str[i])
 	{
-		write(1,&str[i],1);
-	i++;
+		write(1, &str[i], 1);
+		i++;
 	}
 }
 
 void	ft_putnbr(int nb)
 {
-	if (nb ==  -2147483648)
+	if (nb == -2147483648)
 	{
-		write(1,"-2147483648",11);
+		write(1, "-2147483648", 11);
 		return ;
 	}
 	if (nb < 0)
@@ -48,42 +48,51 @@ void	ft_putnbr(int nb)
 }
 
 #define INFO 1
-#define ERROR 2
+#define SUCCESS 2
 #define WARNING 3
+
+void	check_level(int level)
+{
+	if (level == INFO)
+		ft_putstr("[INFO] ");
+	else if (level == SUCCESS)
+		ft_putstr("[SUCCESS] ");
+	else if (level == WARNING)
+		ft_putstr("[WARNING] ");
+}
 
 void	variadic_log_msg(int level, const char *format, ...)
 {
-	int	i;
+	int		i;
 	va_list	args;
 
 	i = 0;
-	va_start(args,format);
-	if (level == INFO)
-		ft_putstr("[INFO] ");
-	else if (level == ERROR)
-		ft_putstr("[ERROR] ");
-	else if (level == WARNING)
-		ft_putstr("[WARNING] ");
+	va_start(args, format);
+	check_level(level);
 	while (format[i] != 0)
 	{
-		if (format[i] == '%' && i != '\0')
+		if (format[i] == '%' && format[i] != '\0')
 		{
-			if (format[i + 1]  == 's')
-				ft_putstr(va_arg(args,char *));
+			if (format[i + 1] == 's')
+				ft_putstr(va_arg(args, char *));
 			else if (format[i + 1] == 'd')
-				ft_putnbr(va_arg(args,int));
+				ft_putnbr(va_arg(args, int));
+			else if (format[i + 1] == '%')
+				ft_putchar('%');
 			else
 				ft_putchar(format[i]);
+			i++;
 		}
 		else
 			ft_putchar(format[i]);
-	i++;
+		i++;
 	}
+	va_end(args);
 }
 
-int main()
+/*int	main(void)
 {
-	variadic_log_msg(INFO, "User %s logged in at %d\n", "john", 10);
-	variadic_log_msg(ERROR, "Failed to open file %s\n", "data.txt");
+	variadic_log_msg(INFO, "User %s logged in cluster at %d\n", "ilsyabri", 10);
+	variadic_log_msg(SUCCESS, "PASS ALL TEST of %s\n", "ft_printf");
 	variadic_log_msg(WARNING, "Memory usage is %d%%\n", 85);
-}
+}*/
