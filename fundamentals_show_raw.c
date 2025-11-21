d2c8f82b10fb63c3e428fcc865c0f0b777cddd3c
ilSyAbRi <iliasssyabri1@gmail.com>
2025-11-21 20:11:29 +0100

    learn to add whit gp_offset

--- fundamentals_show_raw.c
+++ fundamentals_show_raw.c
@@ -6,25 +6,25 @@
 /*   By: ilsyabri <ilsyabri@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 18:09:26 by ilsyabri          #+#    #+#             */
-/*   Updated: 2025/11/21 19:11:18 by ilsyabri         ###   ########.fr       */
+/*   Updated: 2025/11/21 20:11:00 by ilsyabri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdarg.h>
 
-void	fundamentals_show_raw(int x,float k, ...)
+void	fundamentals_show_raw(int x, ...)
 {
 	int	i;
 	va_list args;
 
 	i = 0;
-	va_start(args,k);
+	va_start(args,x);
 	
-	printf("%d\n", args->fp_offset);
-	printf("%d\n", args->gp_offset);
-	printf("%d\n",*(int *)(args->overflow_arg_area));
-	printf("%d\n",*(int *)(args->reg_save_area + 16));
+	printf("%d\n",args->gp_offset);
+	printf("%d\n",args->fp_offset);
+	printf("%d\n",*(int *)args->overflow_arg_area);
+	printf("%d\n",*(int *)(args->reg_save_area + args->gp_offset));
 }
 
 int main()
