2416e41482be59a4febcca0ed76275154d4e5d84
ilSyAbRi <iliasssyabri1@gmail.com>
2025-11-23 18:17:14 +0100

    visit more this concept

--- fundamentals_show_raw_in_variadic_func.c
+++ fundamentals_show_raw_in_variadic_func.c
@@ -6,28 +6,33 @@
 /*   By: ilsyabri <ilsyabri@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 18:09:26 by ilsyabri          #+#    #+#             */
-/*   Updated: 2025/11/21 23:44:08 by ilsyabri         ###   ########.fr       */
+/*   Updated: 2025/11/23 18:16:09 by ilsyabri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdarg.h>
 
-void	fundamentals_show_raw_in_variadic_func(int x, ...)
+void	fundamentals_show_raw_in_variadic_func(double c ,float x, ...)
 {
 	int	i;
 	va_list args;
 
 	i = 0;
-	va_start(args,x);
 	
-	printf("%d\n",args->fp_offset);
-	printf("%d\n",args->gp_offset);
-	printf("%d\n",*(int*)args->overflow_arg_area);
-	printf("%d\n",*(int*)(args->reg_save_area + args->gp_offset));
+	printf("%d\n",args->fp_offset);// no
+	printf("%d\n",args->gp_offset);// no 
+	
+	va_start(args,c);
+	
+	printf("%d\n",args->fp_offset);// 48
+	printf("%d\n",args->gp_offset);// 24 || 16
+
+	//printf("%d\n",*(int *)args->reg_save_area + args->gp_offset);//8  random number
+	//printf("%d\n",*(int *)args->overflow_arg_area);//60 60
 }
 
 int main()
 {
-	fundamentals_show_raw_in_variadic_func(5,20,30,40,50,60,70,80);
+	fundamentals_show_raw_in_variadic_func(5,10,20,30,40,50,60,70);
 }
