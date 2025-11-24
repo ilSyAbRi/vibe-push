f72a8e4f91c4b2925f0c70903c34156a706d6e9d
ilSyAbRi <iliasssyabri1@gmail.com>
2025-11-24 22:31:05 +0100

    improve my function make it more safer and handle negative number

--- variadic_function_find_max.c
+++ variadic_function_find_max.c
@@ -6,34 +6,48 @@
 /*   By: ilsyabri <ilsyabri@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/20 15:39:40 by ilsyabri          #+#    #+#             */
-/*   Updated: 2025/11/20 15:51:09 by ilsyabri         ###   ########.fr       */
+/*   Updated: 2025/11/24 22:29:47 by ilsyabri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int	variadic_function_find_max(int count, ...)
+void	*variadic_function_find_max(int count, ...)
 {
 	int		i;
 	va_list	args;
-	int		max;
+	int		*max;
 	int		keep;
 
-	i = 0;
+	i = 1;
 	va_start(args, count);
-	max = 0;
+	if (count == 0)
+		return (NULL);
+	max = malloc(sizeof(int));
+	if (max == NULL)
+		return (NULL);
+	*max = va_arg(args, int);
 	while (i < count)
 	{
 		keep = va_arg(args, int);
-		if (max < keep)
-			max = keep;
+		if (*max < keep)
+			*max = keep;
 		i++;
 	}
-	return (max);
+	return ((void *)max);
 }
 
 int	main(void)
 {
-	printf("%d\n", variadic_function_find_max(5, 3, 3, 5, 6, 1));
+	int	*nb;
+
+	nb = variadic_function_find_max(5, 3, 3, 5, 6, 1);
+	if (nb == NULL)
+		return (1);
+	else
+		printf("%d\n", *nb);
+	free(nb);
 }
