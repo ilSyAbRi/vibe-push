5c7dbd72a68a3ac642d3e142b73e498f72bc336f
ilSyAbRi <iliasssyabri1@gmail.com>
2025-11-24 22:42:41 +0100

    make my function handle negative number and be more safer and look good at least for me

--- variadic_function_find_min.c
+++ variadic_function_find_min.c
@@ -6,34 +6,50 @@
 /*   By: ilsyabri <ilsyabri@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 22:55:05 by ilsyabri          #+#    #+#             */
-/*   Updated: 2025/11/21 23:26:08 by ilsyabri         ###   ########.fr       */
+/*   Updated: 2025/11/24 22:41:15 by ilsyabri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int	variadic_function_find_min(int count, ...)
+void	*variadic_function_find_min(int count, ...)
 {
 	va_list	args;
 	int		i;
-	int		min;
+	int		*min;
 	int		keep;
 
 	va_start(args, count);
-	i = 0;
-	min = 0;
+	if (count == 0)
+		return (NULL);
+	min = malloc(sizeof(int));
+	if (min == NULL)
+		return (NULL);
+	i = 1;
+	*min = va_arg(args, int);
 	while (i < count)
 	{
 		keep = va_arg(args, int);
-		if (min > keep)
-			min = keep;
+		if (*min > keep)
+			*min = keep;
 		i++;
 	}
-	return (min);
+	return ((void *)min);
 }
 
 int	main(void)
 {
-	printf("%d\n", variadic_function_find_min(5, 1, -2, 3, 0, 5));
+	int	*nb;
+
+	nb = variadic_function_find_min(5, 1, -2, 3, 0, 5);
+	if (nb == NULL)
+		return (1);
+	else
+		printf("%d\n", *nb);
+	free(nb);
+	nb = NULL;
+	return (0);
 }
