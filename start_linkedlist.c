3b0519816906f592ca36482b32ce49ce83ba8f1c
ilSyAbRi <iliasssyabri1@gmail.com>
2026-01-02 13:00:54 +0100

    start understand linked list

---
+++ start_linkedlist.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+typedef struct node
+{
+	int			val;
+	struct node	*next;
+}				node_t;
+
+int	main(void)
+{
+	node_t	*head;
+
+	head = NULL;
+	head = (node_t *)malloc(sizeof(node_t));
+	if (head == NULL)
+	{
+		return (1);
+	}
+	head->val = 1;
+	head->next = NULL;
+}
