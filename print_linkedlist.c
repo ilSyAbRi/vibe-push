1a42874afc18ab2049e43568e2e28ea20b308cfa
ilSyAbRi <iliasssyabri1@gmail.com>
2026-01-04 17:48:41 +0100

    print list

---
+++ print_linkedlist.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct nodes{
+	int	val;
+	struct nodes * next;
+}t_nodes;
+
+t_nodes *create_node_list(void)
+{
+	t_nodes *head = malloc(sizeof(t_nodes));
+	head->val = 1;
+
+	head->next = malloc(sizeof(t_nodes));
+	head->next->val = 2;
+
+	head->next->next = malloc(sizeof(t_nodes));
+	head->next->next->val = 3;
+
+	head->next->next->next = malloc(sizeof(t_nodes));
+	head->next->next->next->val =4;
+
+	head->next->next->next->next = NULL;
+	return head;
+}
+
+void	print_list(t_nodes *head)
+{
+	t_nodes	* current = head;
+
+	while (current != NULL)
+	{
+		printf("%d\n",current->val);
+		current = current->next;
+	}
+}
+
+int main()
+{
+	t_nodes * result = create_node_list();
+
+	print_list(result);
+}
