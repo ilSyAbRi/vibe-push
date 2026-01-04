b920ebec39dd5c867070135f71774949032b86a3
ilSyAbRi <iliasssyabri1@gmail.com>
2026-01-04 17:52:49 +0100

    fix malloc problem

--- print_linkedlist.c
+++ print_linkedlist.c
@@ -9,15 +9,23 @@ typedef struct nodes{
 t_nodes *create_node_list(void)
 {
 	t_nodes *head = malloc(sizeof(t_nodes));
+	if (head == NULL)
+		return NULL;
 	head->val = 1;
 
 	head->next = malloc(sizeof(t_nodes));
+	if (head->next == NULL)
+		return NULL;
 	head->next->val = 2;
 
 	head->next->next = malloc(sizeof(t_nodes));
+	if (head->next->next == NULL)
+		return NULL;
 	head->next->next->val = 3;
 
 	head->next->next->next = malloc(sizeof(t_nodes));
+	if (head->next->next->next == NULL)
+		return NULL;
 	head->next->next->next->val =4;
 
 	head->next->next->next->next = NULL;
