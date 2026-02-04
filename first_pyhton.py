7a6998a5f493b17925427163db30d7481219f849
ilSyAbRi <iliasssyabri1@gmail.com>
2026-02-04 14:36:30 +0100

    start learning python

---
+++ first_pyhton.py
@@ -0,0 +1,11 @@
+
+
+print("1. The program is starting..")
+
+user_name = "Garden Student"
+score = 10 + 32
+
+print (f"2. Welcome, {user_name}")
+print (f"3. Your score is: {score}")
+
+print ("4. The program is finished.")
